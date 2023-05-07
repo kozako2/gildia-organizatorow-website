11772150c611fc913cc0b20b41ee20720c55746e
Aleksander Spyra <aleksander.spyra@gmail.com>
2023-05-07 20:45:53 +0200

    Add preview for carousel tiles

--- assets/js/modalJs.js
+++ assets/js/modalJs.js
@@ -4,9 +4,13 @@ var openedModal = undefined;
 
 function toggleDialog(sh, modalId) {
     console.log("modalId: " + modalId + " sh: " + sh);
+    dialogParent = document.getElementById("memberDetailsModalBackground");
     dialog = document.getElementById(modalId);
     closeButton = dialog.getElementsByClassName("close-button")[0];
     pagebackground = document.getElementById("bg");
+
+    var numberOfTiles = dialogParent.children.length;
+    var index = Array.from(dialogParent.children).indexOf(dialog);
 
     if (sh === "show") {
         lastFocus = document.activeElement;
@@ -15,6 +19,11 @@ function toggleDialog(sh, modalId) {
 
         // show the dialog 
         dialog.style.display = 'block';
+        dialogParent.style.display = 'block';
+
+        // siblings on the left and right
+        displayPreviousTile(index, numberOfTiles);
+        displayNextTile(index, numberOfTiles);
 
         // after displaying the dialog, focus an element inside it
         closeButton.focus();
@@ -23,12 +32,43 @@ function toggleDialog(sh, modalId) {
         pagebackground.setAttribute("aria-hidden", "true");
 
     } else {
+        hidePreviousTile(index, numberOfTiles);
+        hideNextTile(index, numberOfTiles);
         openedModal = undefined;
         dialog.style.display = 'none';
+        dialogParent.style.display = 'none';
         pagebackground.setAttribute("aria-hidden", "false");
         lastFocus.focus();
     }
 }
+
+function displayPreviousTile(index, numberOfTiles) {
+    var previousTileIndex = ((index - 1) % numberOfTiles + numberOfTiles) % numberOfTiles;
+    var previousTile = dialogParent.children[previousTileIndex];
+    previousTile.classList.add("member-details-modal-content-right-preview");
+    previousTile.style.display = "block";
+}
+
+function hidePreviousTile(index, numberOfTiles) {
+    var previousTileIndex = ((index - 1) % numberOfTiles + numberOfTiles) % numberOfTiles;
+    var previousTile = dialogParent.children[previousTileIndex];
+    previousTile.classList.remove("member-details-modal-content-right-preview");
+    previousTile.style.display = "none";
+}
+
+function displayNextTile(index, numberOfTiles) {
+    var nextTileIndex = (index + 1) % numberOfTiles;
+    var nextTile = dialogParent.children[nextTileIndex];
+    nextTile.classList.add("member-details-modal-content-left-preview");
+    nextTile.style.display = "block";
+}
+function hideNextTile(index, numberOfTiles) {
+    var nextTileIndex = (index + 1) % numberOfTiles;
+    var nextTile = dialogParent.children[nextTileIndex];
+    nextTile.classList.remove("member-details-modal-content-left-preview");
+    nextTile.style.display = "none";
+}
+
 
 
 document.addEventListener("focus", function (event) {
